2f7b24c42d0945d3100e8f6b81bd3ed0d6a1048b
Clint Monzon <clint@ativistar.com>
2018-08-05 14:37:02 +0400

    html updated

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Zakkys Group</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="styles.css" rel="stylesheet" />
+
+</head>
+<body>
+
+	<header>
+		<div class="container text-center">
+			<img src="logo.svg" class="logo" />
+			<p>
+				Welcome to Zakky's group of brands.
+			</p>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row brands">
+			<div class="col-6 col-md-6 first">
+				<div class="">
+					<img src="zakkys-chicken.svg" />
+				</div>
+			</div>
+			<div class="col-6 col-md-6 second">
+				<div class="">
+					<img src="zaks-curry.svg" />
+				</div>
+			</div>
+			<div class="col-6 col-md-6 third">
+				<div class="">
+					<img src="zaks-hotdogs.svg" />
+				</div>
+			</div>
+			<div class="col-6 col-md-6 fourth">
+				<div class="">
+					<img src="zaks-burger.svg" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html
